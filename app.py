2cc992360bf2854cf6f0e9567c3121bec10f81c7
ADevito-Spiro <aspiro439@gmail.com>
2024-10-24 11:10:00 -0400

    Create app.py and initial flask app setup.

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import func
+import re
+from decimal import Decimal
+from datetime import datetime
+
+
+app = Flask(__name__)
+app.secret_key = 'GroupLoop'
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
