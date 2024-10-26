d6cc2fd0b1b269584877de9213861ea72502a825
ADevito-Spiro <aspiro439@gmail.com>
2024-10-26 15:26:17 -0400

    Switch to pymysql (personal preference), Update .gitignore for .env file, update README

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ Human Computer Interaction Final Project
 
 ----------------------------------------------------------------
 In your command line, direct to your local directory for the app.
-- First run .venv\Scripts\activate
-- Second run pip install Flask
-- Third run pip install SQLAlchemy
-- Fourth run pip install WTForms
+- First, run .venv\Scripts\activate
+- Second, run pip install Flask
+- Third, run pip install WTForms
+- Fourth, run pip install python-dotenv
+- Fifth, run pip install pymysql
 
 ----------------------------------------------------------------
--- app.py
+++ app.py
@@ -1,16 +1,27 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from wtforms import Form, BooleanField, StringField, validators
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy import func
-import re
+from dotenv import load_dotenv
 from decimal import Decimal
 from datetime import datetime
+import re
+import os
+import pymysql
+
+load_dotenv()
+SQL_USER = os.environ.get('username')
+SQL_PASSWORD = os.environ.get('sql_password')
+SQL_HOST = os.environ.get('sql_host')
+SQL_DB = 'grouploop_db'
 
 app = Flask(__name__)
-app.secret_key = 'GroupLoop'
+app.secret_key = os.urandom(24)
 
-
-
+# PyMySQL documentation.https://pymysql.readthedocs.io/en/latest/
+# This is a personal preference, allows to use written SQL commands using the cursor versus creating an object of SQLAlchemy.
+# Can switch and learn the other if needed but for now going to use this. 
+ 
+def connect_to_database():
+    return pymysql.connect(host=SQL_HOST, user=SQL_USER, password=SQL_PASSWORD, database=SQL_DB)
 
 if __name__ == '__main__':
     app.run(debug=True)
