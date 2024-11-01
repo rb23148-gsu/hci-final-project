d028de71c520512f87e8130d0baae6dee6ee5509
Emman-Ade <146605843+Emman-Ade@users.noreply.github.com>
2024-11-01 13:23:30 -0400

    forms.py

--- forms.py
+++ forms.py
@@ -17,6 +17,22 @@ class LoginForm(FlaskForm):
 class ClassEntryForm(FlaskForm):
     # Use this when we have a table set up with all subject codes, course numbers, and course names.
     # subject_code = SelectField('Subject Code', choices=[], validators=[DataRequired()])
+    section_code = StringField('Section Code')
+
+class CreateAccountForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    username = StringField('Username')
+    first_name = StringField('First Name')
+    last_name = StringField('Last Name')
+    password = PasswordField('Password', validators=[DataRequired()])
+    match_password = PasswordField('Match Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
+    university = SelectField('University', choices=[], validators=[DataRequired()])
+    classes = FieldList(FormField(ClassEntryForm), min_entries=1, max_entries=6)
+    create_account = SubmitField('Create Account')
+
+# ADDING COURSE FORM - Emmanuel
+from wtforms import Form, StringField, SelectField, validators
+
     # course_number = SelectField('Course Number', choices=[], validators=[DataRequired()])
     # section_code = StringField('Section Code', validators=[DataRequired()])
     subject_code = StringField('Subject Code')
@@ -33,4 +49,12 @@ class CreateAccountForm(FlaskForm):
     university = SelectField('University', choices=[], validators=[DataRequired()])
     classes = FieldList(FormField(ClassEntryForm), min_entries=1, max_entries=6)
     create_account = SubmitField('Create Account')
+
+# ADDING COURSE FORM - Emmanuel
+
+
+class CourseForm(Form):
+    course_name = StringField('Course Name', [validators.Length(min=1, max=100)])
+    course_code = StringField('Course Code', [validators.Length(min=1, max=10)])
+    university = SelectField('University', coerce=int)
 
