431773ab46d99e79b724b4dc4f93a4f508d89a6a
rb23148 <117105579+rb23148-gsu@users.noreply.github.com>
2024-11-05 04:20:40 -0500

    Created edit-classes form, conditional nav links, logout route, added style breakpoints
    The edit classes form is on a separate page from the create-account form for reusability. I tried doing it without AJAX since I didn't know anything about it, but I had to learn some stuff. The form currently does not do anything with the data, since that will need to be handled in the edit-classes route. A stub is present to work off of. Conditional nav links show if the user is logged in, but they need additional styling. The logout route works. Breakpoints have been added to the style sheet.
    fixes #8
    fixes #15
    fixes #20

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, jsonify
 from werkzeug.security import generate_password_hash, check_password_hash
 from wtforms import Form, BooleanField, StringField, validators
 from dotenv import load_dotenv
 from decimal import Decimal
 from datetime import datetime
-from forms import LoginForm, CreateAccountForm, CourseForm
+from forms import LoginForm, CreateAccountForm, CourseForm, AddClassesForm
 import re
 import os
 import pymysql
@@ -26,21 +26,27 @@ app.secret_key = os.urandom(24)
 def connect_to_database():
     return pymysql.connect(host=SQL_HOST, user=SQL_USER, password=SQL_PASSWORD, database=SQL_DB)
 
+
 ### BEGIN ROUTE DEFINITIONS ###
 # Set up default route when visiting the site including the login form
 @app.route('/')
 def index():
     # Create form object to be passed to the page.
-    # The login will still be processed in the login route.
+    # Send user to the login page when they visit the app.
+    # The login will be processed in the login route.
     form = LoginForm()
     if 'user' in session:
         return redirect(url_for('dashboard'))
     else:
         return redirect(url_for('login'))
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-
+    # Don't let the user visit the login page if they're already logged in.
+    if 'user' in session:
+        return redirect(url_for('dashboard'))
+    
     # Create form object to be passed to the page.
     form = LoginForm()
     
@@ -61,7 +67,7 @@ def login():
             # This method grabs the password from the provided email or username and checks it later.
             query = "SELECT user_id, password FROM Users WHERE email = %s OR username = %s"
             cursor.execute(query, (input, input))
-            # Catches the result of the last query as a tuple.
+            # Gets the result of the last query as a tuple.
             user = cursor.fetchone()
 
             # Hashed password version
@@ -84,8 +90,14 @@ def login():
     # Render main page and pass the form object to it.
     return render_template('login.html', form=form)
 
+
 @app.route('/create-account', methods=['GET', 'POST'])
 def create_account():
+
+    # Don't let user visit this page if they're already logged in.
+    if 'user' in session:
+        return redirect(url_for('dashboard'))
+    
     # Create form object to pass to the page.
     form = CreateAccountForm()
 
@@ -125,11 +137,10 @@ def create_account():
             session_query = "SELECT user_id FROM Users WHERE email = %s"
             cursor.execute(session_query, (email,))
             user = cursor.fetchone()
-
             session['user'] = user[0]
 
-            # Redirect to the dashboard
-            return redirect(url_for('dashboard'))
+            # Assuming everything else went well, redirect to edit classes page.
+            return redirect(url_for('edit_classes', university_id=university))
 
     except Exception as e:
         flash("There was an error creating the account.")
@@ -143,57 +154,138 @@ def create_account():
             connection.close()
 
     return render_template('create-account.html', form=form)
-    
-#Emmanuel importing Courses into Database
-@app.route('/import-courses', methods=['GET', 'POST'])
-def import_courses():
-    form = CourseForm()
 
-    
-    connection = None
-    cursor = None
 
-    try:
-        # Connection
+@app.route('/edit-classes', methods=['GET', 'POST'])
+def edit_classes():
+
+    # Send the user to the login page if they try to visit this page while not logged in.
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    
+    # Check if the request is an AJAX request for subject codes
+    if request.args.get('action') == 'load_subject_codes':
+        # Get data from the request.
+        university_id = request.args.get('university_id', type=int)
+        # Debugging print
+        print(f"AJAX request for subject codes: university_id={university_id}")
+
+        # Connect to db and get subject codes
         connection = connect_to_database()
         cursor = connection.cursor()
+        cursor.execute("SELECT DISTINCT subject_code FROM Courses WHERE university_id = %s", (university_id,))
+        subject_codes = [code[0] for code in cursor.fetchall()]
+        cursor.close()
+        connection.close()
+        # Make sure we got something back.
+        print("Subject Codes Retrieved:", subject_codes)
+        # Assuming everything worked, send it to populate the field.
+        return jsonify(subject_codes=subject_codes)
 
-        # Getting uni names
-        cursor.execute("SELECT university_id, university_name FROM Universities")
-        universities = cursor.fetchall()
-        form.university.choices = [(uni[0], uni[1]) for uni in universities]
+    # Check if the request is an AJAX request for course numbers
+    if request.args.get('action') == 'load_course_numbers':
+        # Get data from the request.
+        university_id = request.args.get('university_id', type=int)
+        subject_code = request.args.get('subject_code')
+        # Debug because getting this to work sucked.
+        print(f"AJAX request for course numbers: university_id={university_id}, subject_code={subject_code}")
 
-        if form.validate_on_submit():
-            course_name = form.course_name.data
-            course_code = form.course_code.data
-            university_id = form.university.data
+        # Connect to db and get course numbers.
+        connection = connect_to_database()
+        cursor = connection.cursor()
+        cursor.execute("SELECT course_number FROM Courses WHERE university_id = %s AND subject_code = %s", (university_id, subject_code))
+        course_numbers = [num[0] for num in cursor.fetchall()]
+        cursor.close()
+        connection.close()
+        # Print course numbers to make sure it worked.
+        print("Course Numbers Retrieved:", course_numbers)
+        # Send course numbers that exist for the subject code.
+        return jsonify(course_numbers=course_numbers)
+    
+    # Do this once the form is submitted.
+    # if form.validate_on_submit():
+        # Get input data from the form submission. Use a for loop.
+        # Section data will go into the Sections table to register that the section and course combo exists. Querying groups will use this data.
+        # We are relying on the user to get the section data correct.
+        # If the section/course combo exists, skip putting it in the db.
+        # Next, the Enrollments table needs to be used to tie the user to the course/section combos.
+        # Once this is done, we can redirect to the dashboard.
+        # Did I miss anything?
 
-            # If valid, insert course data into database
-            query = "INSERT INTO Courses (course_name, course_code, university_id) VALUES (%s, %s, %s)"
-            cursor.execute(query, (course_name, course_code, university_id))
-            connection.commit()
+        # This worked for an earlier version before AJAX, but a lot has changed. I haven't tested it yet.
+        # # Iterate over class_entry data from multiple classes to get all entries.
+        # for class_entry in form.classes:
+        #     subject_code = class_entry.subject_code.data
+        #     course_number = class_entry.course_number.data
+        #     section_code = class_entry.section_code.data
 
-            flash("Course imported successfully.")
-            return redirect(url_for('import_courses'))
+        # Redirect to edit classes page.
+        return redirect(url_for('dashboard'))
 
-    except Exception as e:
-        flash("There was an error importing the course.")
-        print(f"Error importing course: {e}")
+    # Regular page load or form submission
+    form = AddClassesForm()
+    university_id = request.args.get('university_id', type=int)
+    return render_template('edit-classes.html', form=form, university_id=university_id)
 
-    finally:
-        # Closing cursor and connection
-        if cursor:
-            cursor.close()
-        if connection:
-            connection.close()
+# Rob - The course data is in its own file. Unless we're going to manually input 
+# the courses in through the website, this isn't needed, but you could probably repurpose some of it
+# to use it with the pdf/txt file. 
+#Emmanuel importing Courses into Database
+# @app.route('/import-courses', methods=['GET', 'POST'])
+# def import_courses():
+#     form = CourseForm()
 
-    return render_template('import_courses.html', form=form)
+    
+#     connection = None
+#     cursor = None
+
+#     try:
+#         # Connection
+#         connection = connect_to_database()
+#         cursor = connection.cursor()
+
+#         # Getting uni names
+#         # cursor.execute("SELECT university_id, university_name FROM Universities")
+#         # universities = cursor.fetchall()
+#         # form.university.choices = [(uni[0], uni[1]) for uni in universities]
+
+#         if form.validate_on_submit():
+#             course_name = form.course_name.data
+#             course_code = form.course_code.data
+#             university_id = form.university.data
+
+#             # If valid, insert course data into database
+#             query = "INSERT INTO Courses (course_name, course_code, university_id) VALUES (%s, %s, %s)"
+#             cursor.execute(query, (course_name, course_code, university_id))
+#             connection.commit()
+
+#             flash("Course imported successfully.")
+#             return redirect(url_for('import_courses'))
+
+#     except Exception as e:
+#         flash("There was an error importing the course.")
+#         print(f"Error importing course: {e}")
+
+#     finally:
+#         # Closing cursor and connection
+#         if cursor:
+#             cursor.close()
+#         if connection:
+#             connection.close()
+
+#     return render_template('import_courses.html', form=form)
     
 @app.route('/dashboard')
 def dashboard():
     # For now, just render the dashboard to show we can go there after logging in
-    # We still need to get users logged in and session data going
     return render_template('dashboard.html')
+
+@app.route('/logout', methods=['POST'])
+def logout():
+        # Clear session to log out the user and send them to the login.
+        session.clear()
+        flash('You have been logged out!')
+        return redirect(url_for('login'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, FieldList, FormField
+from wtforms import StringField, PasswordField, SubmitField, SelectField, FieldList, FormField, validators
 from wtforms.validators import DataRequired, Regexp, Email, EqualTo
 
 ### LOGIN FORM ###
@@ -16,9 +16,12 @@ class LoginForm(FlaskForm):
 # So we create a subform to be included in the main registration form.
 class ClassEntryForm(FlaskForm):
     # Use this when we have a table set up with all subject codes, course numbers, and course names.
-    # subject_code = SelectField('Subject Code', choices=[], validators=[DataRequired()])
-    section_code = StringField('Section Code')
+    # Variable names correspond to columns in Courses table.
+    subject_code = SelectField('Subject Code', choices=[], validators=[DataRequired()])
+    course_number = SelectField('Course Number', choices=[], validators=[DataRequired()])
+    section_code = StringField('Section Code', validators=[DataRequired()])
 
+# Form to create the account on create-account page.
 class CreateAccountForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     username = StringField('Username')
@@ -27,16 +30,16 @@ class CreateAccountForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     match_password = PasswordField('Match Password', validators=[DataRequired(), EqualTo('password', message='Passwords must match.')])
     university = SelectField('University', choices=[], validators=[DataRequired()])
-    classes = FieldList(FormField(ClassEntryForm), min_entries=1, max_entries=6)
     create_account = SubmitField('Create Account')
 
-
+# Form for the edit-classes page using the ClassEntryForm subform.
+class AddClassesForm(FlaskForm):
+    classes = FieldList(FormField(ClassEntryForm), min_entries=1, max_entries=6)
+    add_classes = SubmitField('Add All Classes to Account')
 
 
 # ADDING COURSE FORM - Emmanuel
-
-
-class CourseForm(Form):
+class CourseForm(FlaskForm):
     course_name = StringField('Course Name', [validators.Length(min=1, max=100)])
     course_code = StringField('Course Code', [validators.Length(min=1, max=10)])
     university = SelectField('University', coerce=int)
